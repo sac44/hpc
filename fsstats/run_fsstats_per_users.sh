f0492dfb146f08105e318f20b7c9859687bccc92
Steven Chapman <36702700+sac44@users.noreply.github.com>
2026-06-17 11:54:55 +0100

    Add files via upload

---
+++ fsstats/run_fsstats_per_users.sh
@@ -0,0 +1,14 @@
+dir=/cm/shared/bath/scripts/fsstats/per-user/$(date +%Y-%m-%d)
+mkdir -p $dir
+log=$dir/fsstats_progress.log
+err=$dir/fsstats_progress.err
+for d in $(ls -d /beegfs/scratch/user/[a-z]/*)
+#        do f=$(echo $d | cut -c2- | tr '/' '-')
+        do f=$(echo $d | cut -c2- | tr '/' '_')_$(ls -dn $d | awk '{print $3}')
+	echo $d $f >> $log
+
+        ($dir/../../fsstats.pl -o $dir/$f.csv $d > $dir/$f.log) 2> $err
+
+done
+
+
